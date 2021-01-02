be45d4bc7519140bfe86283bd21a1521cc69c93d
Juan Fajardo <juanfar.group@gmail.com>
2021-01-02 22:26:54 +0100

    angular fire configuration

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,4 +1,14 @@
 export const environment = {
   production: false,
-  name: "dev"
+  name: 'dev',
+  firebase: {
+    config: {
+      apiKey: 'AIzaSyAgTPgLc8ulPRTdRqaiG0Bz72JSpiUbX7Y',
+      authDomain: 'courseapp-b05f3.firebaseapp.com',
+      projectId: 'courseapp-b05f3',
+      storageBucket: 'courseapp-b05f3.appspot.com',
+      messagingSenderId: '179852081519',
+      appId: '1:179852081519:web:aee1a8fd446b692ec5109a'
+    }
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,18 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  name: "default",
+  firebase: {
+    config: {
+      apiKey: 'AIzaSyAgTPgLc8ulPRTdRqaiG0Bz72JSpiUbX7Y',
+      authDomain: 'courseapp-b05f3.firebaseapp.com',
+      projectId: 'courseapp-b05f3',
+      storageBucket: 'courseapp-b05f3.appspot.com',
+      messagingSenderId: '179852081519',
+      appId: '1:179852081519:web:aee1a8fd446b692ec5109a'
+    }
+  }
 };
 
 /*
