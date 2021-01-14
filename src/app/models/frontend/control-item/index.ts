435b5fb8aef09ea1fe81fd0c490306001e7b8b24
Juan Fajardo <juanfar.group@gmail.com>
2021-01-14 19:51:08 +0100

    fontend models was creates

---
+++ src/app/models/frontend/control-item/index.ts
@@ -0,0 +1,11 @@
+import { ValueConverter } from "@angular/compiler/src/render3/view/template";
+
+import { Icon } from '../icon'
+
+export type Value = number | string | boolean;
+
+export interface ControlItem {
+  value: Value;
+  label: string;
+  icon: Icon;
+}
