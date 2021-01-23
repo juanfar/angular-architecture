a1821dd8c4cb9358338357c18701b0149c33eae1
Juan Fajardo <juanfar.group@gmail.com>
2021-01-23 19:53:22 +0100

    Effects: SignUp

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -9,6 +9,10 @@ export const environment = {
       storageBucket: 'courseapp-b05f3.appspot.com',
       messagingSenderId: '179852081519',
       appId: '1:179852081519:web:aee1a8fd446b692ec5109a'
+    },
+    actionCodeSettings: {
+      url: 'http://localhost:5200/demo',
+      handleCodeInApp: true
     }
   }
 };
